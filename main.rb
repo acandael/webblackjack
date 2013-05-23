55b0bfec00fb678ce93146e8cad70dda252d4f48
Anthony Candaele <anthony.candaele@webmove.be>
2013-05-23 21:28:36 +0200

    add bet functionality

--- main.rb
+++ main.rb
@@ -17,6 +17,7 @@ end
 
 post'/new_player' do
   session[:player_name] = params[:player_name]
+  
   redirect '/game'
 end
 
@@ -33,5 +34,64 @@ get '/game' do
   session[:dealer_cards] << session[:deck].pop
   session[:player_cards] << session[:deck].pop
 
+  session[:player_credit] = 500
+
   erb :game
+end
+
+post '/hit' do
+  session[:player_credit] -= params[:place_bet].to_i
+  session[:player_cards] << session[:deck].pop
+  "the players credit is #{session[:player_credit]}"
+end
+
+def to_image(card)
+  suit = card[0].to_s
+  value = card[1].to_s
+
+  case suit
+  when "H"
+    suit = "hearts"
+  when "D"
+    suit = "diamonds"
+  when "S"
+    suit = "spades"
+  when "C"
+    suit = "clubs"
+  end
+
+  case value
+  when "J"
+    value = "jack"
+  when "Q"
+    value = "queen"
+  when "K"
+    value = "king"
+  when "A"
+    value = "ace"
+  end
+  "#{suit}_#{value}.jpg"
+end
+
+def total(cards)
+  # [['H', '3'], ['S', 'Q'], ... ]
+  arr = cards.map{|e| e[1] }
+
+  total = 0
+
+  arr.each do |value|
+    if value == "A"
+      total += 11
+    elsif value.to_i == 0 # J, Q, K
+      total += 10
+    else
+      total += value.to_i
+    end
+  end
+
+  #correct for Aces
+  arr.select{|e| e == "A"}.count.times do
+    total -= 10 if total > 21
+  end
+  total
 end
